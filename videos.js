4aec69c0a8e0276f1880535aff8f6b38aff662ff
Munetios <minecraftgamer97529@gmail.com>
2026-08-03 23:33:51 -0400

    Removed some videos that are hidden in restricted mode to keep Munetios safe

--- videos.js
+++ videos.js
@@ -38,32 +38,20 @@ window.MUNETIOS_VIDEOS = [
     "IF I LAUGH = YOU GET ROBUX",
     "Trending on YouTube Gaming · Ninjagaming",
   ],
-  [
-    "i2a_AiMq7lk",
-    "LIVE | PAYDAY 3 | GIMMICK X JOHN | #BUNGULATE",
-    "Trending on YouTube Gaming · TheBurntPeanut",
-  ],
+
   [
     "nkTU3MPySR0",
     "THERES IS 0 PERCENT CHANCE IM SURVIVING THIS GAME OR ITS JUMPSCARES | Welcome To The Game 3",
     "Trending on YouTube Gaming · Kubz Scouts",
   ],
-  [
-    "5atQI_x55L0",
-    "It was a GENIUS Idea to play this...",
-    "Trending on YouTube Gaming · 3FS",
-  ],
+
   [
     "iM5d9Cs8D1c",
     "5,800,000 SUBSCRIBERS LIVE",
     "Trending on YouTube Gaming · Brandon302",
   ],
   ["dozrn3U4A04", "The Ice Cream Man…", "Trending on YouTube Gaming · CaseOh"],
-  [
-    "Ls4SsVsvy_E",
-    "How long until Goofy takes over my stream?",
-    "Trending on YouTube Gaming · DougDougDoug",
-  ],
+
   [
     "U_dV5Qfabgw",
     "I Found A VERITY Key In +1 Keyboard Escape...",
@@ -89,31 +77,15 @@ window.MUNETIOS_VIDEOS = [
     "I Secretly Duped EVERY Item in Minecraft",
     "Trending on YouTube Gaming · Bionic",
   ],
-  [
-    "OsVMNBvQPNg",
-    "Trolling as VERITY on Roblox Voice Chat..",
-    "Trending on YouTube Gaming · nobrainjames",
-  ],
-  [
-    "tQgBxnjjWr8",
-    "Playing DONUT SMP for the first time..",
-    "Trending on YouTube Gaming · steak",
-  ],
+
+
   [
     "PmfBH9pBccM",
     "I Got The Most OP Weapon in +1 Ammo Per Click in Roblox",
     "Trending on YouTube Gaming · SSundee",
   ],
-  [
-    "mIBBjoPqEvA",
-    "We Became Storm Chasers...",
-    "Trending on YouTube Gaming · Yumi",
-  ],
-  [
-    "QRiug3vfSoc",
-    "People are HACKING on BLACK OPS 2..",
-    "Trending on YouTube Gaming · JEV",
-  ],
+
+
   [
     "piZvyD0Ilv4",
     "ROBLOX RUN FOR SQUISHY DUMPLING!",
@@ -124,21 +96,8 @@ window.MUNETIOS_VIDEOS = [
     "Trolling My Friends In Rivals Prop Hunt...",
     "Trending on YouTube Gaming · Techy Plays",
   ],
-  [
-    "uMMyozRqglI",
-    "Meccha Chameleon Spots That Are a Work of Fart",
-    "Trending on YouTube Gaming · WILDCAT",
-  ],
-  [
-    "MuRA17pWI0g",
-    "Flats OFFICIAL Marvel Rivals Season 9 Hero Tier List",
-    "Trending on YouTube Gaming · Flats Two",
-  ],
-  [
-    "mJOHagSHkU4",
-    "This New Recruit Could Change Everything! Sea Otters Ep. #2",
-    "Trending on YouTube Gaming · MMG",
-  ],
+
+
   [
     "2ENxq9jTufM",
     "Minecraft TREE HOUSE NOOB vs PRO vs HACKER...",
@@ -149,21 +108,9 @@ window.MUNETIOS_VIDEOS = [
     "We Pranked the STRICTEST BABYSITTER in Minecraft!",
     "Trending on YouTube Gaming · Johnny Minecraft",
   ],
-  [
-    "J8Jt6OE-U38",
-    "using OG DEMON LOADOUTS on Warzone",
-    "Trending on YouTube Gaming · Lucky Chamu",
-  ],
-  [
-    "Km6IssYFdwA",
-    "Returning to PUBG Mobile: Unleashing Raw Skills After 2.5 Years!",
-    "Gaming Dada · PUBG Mobile comeback gameplay",
-  ],
-  [
-    "wFV6IERdA0A",
-    "Isse jaada fun kahi nahi milega || Funniest video of #pubgmobile Ever || 😂😂",
-    "Gaming Dada · PUBG Mobile funny moments",
-  ],
+
+
+
   [
     "vCF_T7drOBY",
     "Nokia ka jamana wapas aagaya - || Redball 4 gameplay || 💜 - Android gameplay",
